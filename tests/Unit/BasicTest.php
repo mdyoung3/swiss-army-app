07799b8e9c5b3fc5b56aa60aa3015cfb09769987
Marc Young <marcdeeyoung@gmail.com>
2025-11-05 13:57:51 -0700

    Basic tests

--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BasicTest extends TestCase
 {
     use RefreshDatabase;
 
